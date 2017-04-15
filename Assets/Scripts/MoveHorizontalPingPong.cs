5f402be69a3281ed975b2f639c41baec15ed2d65
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-04-15 09:32:05 -0300

    Criando script para efeito de movimentação 'Ping Pong' na horizontal.

---
+++ Assets/Scripts/MoveHorizontalPingPong.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHorizontalPingPong : MonoBehaviour {
+
+	public float speed = 1;
+
+	void Start()
+	{
+		SetHorizontalSpeed ();
+	}
+
+	void FixedUpdate()
+	{
+		if (transform.position.x >= 4.3f || transform.position.x <= -4.3f)
+		{
+			speed *= -1;
+			SetHorizontalSpeed ();
+		}
+	}
+
+	//define a velocidade e direção horizontal
+	void SetHorizontalSpeed()
+	{
+		GetComponent<Rigidbody> ().velocity = transform.right * speed;
+	}
+}
