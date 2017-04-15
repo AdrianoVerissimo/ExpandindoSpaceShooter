ca61af61bd4154aa42d6a9602dd16796db0879d0
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-04-15 10:20:54 -0300

    Adicionando tiros variados ao primeiro chefão.

--- Assets/Scripts/WeaponControllerBoss1.cs
+++ Assets/Scripts/WeaponControllerBoss1.cs
@@ -5,21 +5,59 @@ using UnityEngine;
 public class WeaponControllerBoss1 : MonoBehaviour {
 
 	public GameObject shot;
-	public Transform shotSpawn;
+	public Transform[] shotSpawn;
 	public float fireRate;
-	public float delay;
+	public float delayBeforeShots;
+	public float pauseAfterShots;
+	public int numberOfShots = 5;
 
+	private bool shotSide = false;
+	private bool shotMiddle = true;
 	private AudioSource audioSource;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		if (shotSpawn.Length == 0)
+			return;
+		
 		audioSource = GetComponent<AudioSource> ();
-		InvokeRepeating ("Fire", delay, fireRate);
+		StartCoroutine (Fire ());
+		//InvokeRepeating ("Fire", delay, fireRate);
 	}
 
-	void Fire()
+	IEnumerator Fire()
 	{
-		Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
-		audioSource.Play ();
+		yield return new WaitForSeconds (delayBeforeShots); //espera X segundos para começar
+
+		while (true)
+		{
+			for (int count = 0; count < numberOfShots; count++)
+			{
+				if (shotSide)
+				{
+					Instantiate (shot, shotSpawn [0].position, shotSpawn [0].rotation);
+					Instantiate (shot, shotSpawn [1].position, shotSpawn [1].rotation);
+				}
+				else if (shotMiddle)
+				{
+					Instantiate (shot, shotSpawn [2].position, shotSpawn [2].rotation);
+					Instantiate (shot, shotSpawn [3].position, shotSpawn [3].rotation);
+				}
+				audioSource.Play ();
+
+				yield return new WaitForSeconds (fireRate); //espera X segundos para começar
+			}
+
+			ChangeShotType ();
+
+			yield return new WaitForSeconds (pauseAfterShots); //espera X segundos para começar
+		}
+	}
+
+	void ChangeShotType()
+	{
+		shotSide = !shotSide;
+		shotMiddle = !shotMiddle;
 	}
 }
