a2b684a9c552b37e6dd530206b6bc48bc402b26d
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-05-01 08:54:26 -0300

    Exibindo recorde no menu inicial.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public DataController dataController;
+	public Text highScoreLabel;
+
+	private int highScore;
+
+
+	// Use this for initialization
+	void Start () {
+
+		dataController.LoadPlayerProgress ();
+
+		highScore = dataController.GetHighScore ();
+
+		highScoreLabel.text = "Recorde: " + highScore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
