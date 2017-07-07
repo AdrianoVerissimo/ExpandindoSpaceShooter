ab713913405b066cb0354e99c86d0715736143b9
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-07-07 10:56:14 -0300

    Aplicando menu de pause durante o jogo.

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+	public Canvas canvasPause;
+
+	private bool paused = false;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			Pause (!paused);
+		}
+	}
+
+	public void Unpause()
+	{
+		Pause (false);
+	}
+
+	public void Pause(bool value = true)
+	{
+		paused = value;
+
+		if (paused)
+		{
+			Time.timeScale = 0;
+			canvasPause.gameObject.SetActive (true);
+		}
+		else
+		{
+			Time.timeScale = 1;
+			canvasPause.gameObject.SetActive (false);
+		}
+	}
+
+	public bool GetPaused()
+	{
+		return paused;
+	}
+}
