98c707d7786f118e86378b7bfdbf8bd6ccbbbca8
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-07-21 14:22:30 -0300

    Reajustando design do menu 'Como Jogar'.

---
+++ Assets/Scripts/ButtonAutoSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ButtonAutoSelect : MonoBehaviour {
+
+	private Button button;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button> ();
+
+		button.Select ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
